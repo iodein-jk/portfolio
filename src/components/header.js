944d25884f100e8fe1c20ce0615c6b74b7605ea1
iodein <ken.record1210@gmail.com>
2023-06-05 17:19:36 +0900

    ナビのリンクを追加

--- src/components/header.js
+++ src/components/header.js
@@ -12,6 +12,12 @@ const Header = ({ siteTitle }) => (
         <li><Link className="px-5 text-sky-400" to="/">Top</Link></li>
         <li><Link className="px-5 text-sky-400" to="/about">About</Link></li>
         <li><Link className="px-5 text-sky-400" to="/works">Works</Link></li>
+        <li><a class="px-5 text-sky-400 flex items-center" href="https://iodein-jk.github.io/library/" target="_blank">
+          Library
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 16.5h2.25m8.25-8.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-7.5A2.25 2.25 0 018.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 00-2.25 2.25v6" />
+          </svg>
+          </a></li>
       </ul>
     </nav>
   </header>
--- src/pages/about.js
+++ src/pages/about.js
@@ -45,6 +45,19 @@ const SecondPage = () => (
                         </a>
                       </li>
                       <li class="my-3">
+                        <a href='https://iodein-jk.github.io/library/' target="_blank" class="flex items-center text-sky-400 hover:underline">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                          </svg>
+                          <span class="block">
+                            個人作成のjsライブラリサイトはこちら
+                          </span>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 16.5h2.25m8.25-8.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-7.5A2.25 2.25 0 018.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 00-2.25 2.25v6" />
+                          </svg>
+                        </a>
+                      </li>
+                      <li class="my-3">
                         <a href={Pdf} target="_blank" class="flex items-center text-sky-400 hover:underline">
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
